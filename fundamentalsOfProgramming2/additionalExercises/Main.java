26ce6ef6dd9e862296d88e8b0abb5e12ef9b0ee5
denisjur <denis.f2020@yahoo.com>
2025-03-20 18:50:19 +0100

    first Java program

---
+++ fundamentalsOfProgramming2/additionalExercises/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+					
+		//Without new line
+		System.out.print("I love pizza");
+		System.out.print("It's really good");
+		
+		//Generate System.out.println() like this: sysout + Ctrl + Space + Enter
+		
+		//Alternatives with new line
+		System.out.print("\nI love pizza\n");
+		System.out.println("It's really good");
+		System.out.print("\t I love pizza\n");
+		
+		//If you want do display a quote
+		System.out.println("\"Some Quote\"");
+		
+		// I you want to use a backslash, you use two backslashes:
+		System.out.println("\\Some Quote\\");
+		System.out.println();
+	}
+
+}
